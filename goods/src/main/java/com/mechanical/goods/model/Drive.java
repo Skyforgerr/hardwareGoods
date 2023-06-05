6f130fdfbacd4ee979feb3c9416595fe5f1d82e0
Skyforgerr <postnikoviivan@gmail.com>
2023-06-05 22:15:59 +0300

    База данных изменена на PostgreSQL, так как есть проблемы с H2

---
+++ goods/src/main/java/com/mechanical/goods/model/Drive.java
@@ -0,0 +1,29 @@
+package com.mechanical.goods.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Drive {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  // Общие поля
+  private String series;
+  private String manufacturer;
+  private float cost;
+  private int amountOfGoodsLeftInTheStorage;
+  // Дополнительное поле
+  private int value;
+
+}
---
+++ goods/src/main/java/com/mechanical/goods/model/Laptop.java
@@ -0,0 +1,28 @@
+package com.mechanical.goods.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Laptop {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  // Общие поля
+  private String series;
+  private String manufacturer;
+  private float cost;
+  private int amountOfGoodsLeftInTheStorage;
+  // Дополнительное поле
+  private int size;
+}
